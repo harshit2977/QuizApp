2bc4082b532276c2da1782b89785203835e11086
harshit2977 <harshit2977@gmail.com>
2023-01-17 22:59:45 +0530

    QuizApp

---
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myquizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.accessibility.AccessibilityManagerCompat.TouchExplorationStateChangeListener
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnstart:Button=findViewById(R.id.btn_start)
+        val etname:EditText=findViewById(R.id.et_name)
+        btnstart.setOnClickListener{
+            if(etname.text.isEmpty()){
+                Toast.makeText(this,"please enter your name",Toast.LENGTH_LONG).show()
+
+            }
+            else{
+                val intent=Intent(this,QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME,etname.text.toString())
+                startActivity(intent)
+                finish()
+
+            }
+        }
+    }
+}
